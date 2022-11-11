826448c9cdced67ca607a6cb86ee80496c5e618b
alvaro hernandez <alvarohernandezmurcia@gmail.com>
2022-11-11 17:16:20 +0100

    casi2

--- ws/modifyElements.php
+++ ws/modifyElements.php
@@ -7,12 +7,12 @@ include_once "Respuesta.php";
 $conect = Conexion::conectar();
 $miPdo = $conect->getPdo();
 
-$id = $_GET['id'] ?? null;
-$name = $_POST["nombre"] ?? "PC";
-$description = $_POST["desc"] ?? "GAMING";
-$serialNumber = $_POST["numSer"] ?? "1234";
-$condition =  $_POST["estado"] ?? "ActivoPAPI";
-$priority =  $_POST["igual"];
+$id = $_GET['id'];
+$name = $_POST["nombre"]?? "patata";
+$description = $_POST["desc"] ?? "gaming";
+$serialNumber = $_POST["numSer"]?? "1234";
+$condition =  $_POST["estado"]?? "jodido";
+$priority =  $_POST["igual"]?? null;
 switch ($priority) {
     case "Bajo":
         $priority = "Baja";
@@ -24,24 +24,18 @@ switch ($priority) {
         $priority = "Alta";
     break;
     //Si lo dejo, siempre me va a coger la por defecto
-    /* default:
-        $priority = "simrep"; 
-    break;*/
+     default:
+        $priority = "sinrep"; 
+    break;
 }
 
 
 //comprobacion para saber si le has pasado un id
 if(!empty($_GET['id'])){
-
     $respuesta = $miPdo->prepare('SELECT COUNT(*) as filas FROM elementos WHERE id = :id');
     $respuesta->bindParam(':id', $id, PDO::PARAM_INT);
     $respuesta->execute();
-    
     if($respuesta->fetchColumn() > 0){
-
-        
-        //print_r(Respuesta::mensaje(True, "Usuario encontrado (DATOS ANTIGUOS)", $comprobacionExistencia));
-
         //Una vez que nos ha mostrado los datos, que los nuevos introducidos nos los cambie en la db
         $consultaUpdate = ("UPDATE elementos SET 
                             nombre = :nombre, 
@@ -49,14 +43,10 @@ if(!empty($_GET['id'])){
                             nserie = :numSer,
                             estado = :estado, 
                             prioridad = :igual where id = :id");
-
         $comprobarUpdate = modificarDatos($miPdo, $consultaUpdate, $name, $description, $serialNumber, $condition, $priority, $id);
         $consultarExistencia = "SELECT * FROM elementos WHERE id = ?";
-
         $comprobacionExistencia = modificarSiExiste($miPdo, $consultarExistencia, $id);
         print_r(Respuesta::mensaje(True, "Usuario encontrado y datos actualizados", $comprobacionExistencia));
-
-
     }else{
         print_r(Respuesta::mensaje(False, "El id introducido no existe en la base de datos", null));
     }
@@ -80,7 +70,6 @@ function modificarSiExiste($pdo, $consultaAEjecutar, $id){
 
 
 function modificarDatos($pdo, $consultaUpdate, $name, $description, $serialNumber, $condition, $priority, $id){
-
     $consultaUpdate = ("UPDATE elementos SET 
                             nombre = :nombre, 
                             descripcion = :desc, 
@@ -97,39 +86,42 @@ function modificarDatos($pdo, $consultaUpdate, $name, $description, $serialNumbe
     $consultaAEjecutar->bindParam(':igual', $priority, PDO::PARAM_STR);  
     $consultaAEjecutar->bindParam(':id', $id, PDO::PARAM_INT);
     $exe = $consultaAEjecutar->execute();
+}  
 
+
+
+
+// PRUEBAS PARA QUE CUANDO UN CAMPO ESTÉ VACIO QUE LO DEJE COMO ESTABA
+
+/* function modificarDatos($pdo, $consultaUpdate, $name, $description, $serialNumber, $condition, $priority, $id){
+
+    $consultaUpdate = ("UPDATE elementos SET 
+                            nombre = :nombre, 
+                            descripcion = :desc, 
+                            nserie = :numSer,
+                            estado = :estado, 
+                            prioridad = :igual where id = :id");
+
+    $consultaAEjecutar = $pdo->prepare($consultaUpdate);
+    
+    
+    $consultaAEjecutar->bindParam(':desc', $description, PDO::PARAM_STR);
+    $consultaAEjecutar->bindParam(':numSer', $serialNumber, PDO::PARAM_STR);
+    $consultaAEjecutar->bindParam(':estado', $condition, PDO::PARAM_STR);
+    $consultaAEjecutar->bindParam(':igual', $priority, PDO::PARAM_STR);  
+    $consultaAEjecutar->bindParam(':id', $id, PDO::PARAM_INT);
+    
 // SI ALGUNO NO VIENE POR EL IMPUT DEJARLO COMO ESTÁ
 
-
-
-
-
-
-
-
+if(!empty($name = $_POST["nombre"])){
+    $consultaAEjecutar->bindParam(':nombre', $name, PDO::PARAM_STR);
+}else{
+    
 }
 
 
+$exe = $consultaAEjecutar->execute();
+
+}  */
 
 
-
-
-
-
-
-
-
-/////////////////////////////
-
-
-
-/* $consulta = "UPDATE elementos SET 
-            nombre=':nombre', 
-            descripcion=':descripcion', 
-            numero de serie=':serialNumber',
-            estado=':estado', 
-            prioridad=':prioridad' where id = ':id' ";
-
-
-$consultaUpdate = $miPdo -> prepare($consulta);
-$consultaUpdate->execute($arrayElements); */
